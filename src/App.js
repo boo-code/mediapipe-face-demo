5846e8fbd351a3d7e5188e17041867b9cf3fbd3e
furkandindar <fdindar1@binghamton.edu>
2021-11-10 15:43:14 +0300

    add custom eye tracking

--- src/App.js
+++ src/App.js
@@ -5,32 +5,43 @@ import * as cam from "@mediapipe/camera_utils";
 import Webcam from "react-webcam";
 import {useRef, useEffect} from 'react';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { Paper, Typography, Grid } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import { Grid } from '@mui/material';
 import Header from './Header';
-
-
-function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            QReal MediaPipe FaceMesh Demo
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Suspense } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "@react-three/drei";
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
+
+function Model(props) {
+  const gltf = useLoader(GLTFLoader, "models/anaconda_black.glb");
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.position.x = (landmark_x - 0.5)*10;
+    ref.current.position.y = -(landmark_y - 0.5)*7.5;
+    ref.current.position.z = -(landmark_z);
+    ref.current.scale.x = scale_x*100;
+    ref.current.scale.y = scale_x*100;
+    console.log(ref.current);
+  });
+  return (
+    <>
+      <primitive {...props} ref={ref} object={gltf.scene} scale={18}></primitive>
+    </>
+  );
+}
+
+var landmark_x = 0;
+var landmark_y = 0;
+var landmark_z = 0;
+var scale_x = 0;
+var scale_y = 0;
 
 function App() {
 
@@ -40,6 +51,24 @@ function App() {
   const connect = window.drawConnectors;
 
   function onResults(results){
+    //console.log(Facemesh.FACEMESH_RIGHT_EYE);
+
+    if(results.multiFaceLandmarks){
+      for (const landmarks of results.multiFaceLandmarks) {
+        if(landmarks[0].x !== undefined){
+          landmark_x = (landmarks[446].x + landmarks[226].x)/2;
+          landmark_y = (landmarks[101].y + landmarks[330].y)/2;
+          landmark_z = landmarks[5].z;
+          scale_x = landmarks[265].x - landmarks[35].x;
+          scale_y = landmarks[446].y - landmarks[226].y;
+        }
+      }
+      //console.log("x coordinate: " + landmark_x);
+      //console.log("y coordinate: " + landmark_y);
+      //console.log("z coordinate: " + landmark_z);
+      //console.log(scale_x);
+      //console.log(landmark_z);
+    }
 
     //Setting height and width of canvas
     canvasRef.current.width = webcamRef.current.video.videoWidth;
@@ -52,10 +81,10 @@ function App() {
     canvasCtx.clearRect(0,0,canvasElement.width,canvasElement.height);
     canvasCtx.drawImage(results.image,0,0,canvasElement.width,canvasElement.height);
 
+    //console.log(Facemesh.FACEMESH_FACE_OVAL);
+
     if(results.multiFaceLandmarks){
       for (const landmarks of results.multiFaceLandmarks) {
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION,
-                       {color: '#C0C0C070', lineWidth: 1});
         connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {color: '#30FF30', lineWidth: 1});
         connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {color: '#30FF30', lineWidth: 1});
         connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_IRIS, {color: '#30FF30', lineWidth: 1});
@@ -79,11 +108,12 @@ function App() {
       maxNumFaces: 1,
       refineLandmarks: true,
       minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+      minTrackingConfidence: 0.5,
+      selfieMode: true,
     });
-  
+
     faceMesh.onResults(onResults);
-  
+
     if(typeof webcamRef.current!=="undefined" && webcamRef.current!==null){
       camera = new cam.Camera(webcamRef.current.video,{
         onFrame:async()=>{
@@ -94,6 +124,8 @@ function App() {
     }
   }, []);
 
+  //landmarks_test[1],landmarks_test[2],landmarks_test[3]
+
   return (
       <ThemeProvider theme={darkTheme}>
         <Grid container direction="column">
@@ -101,13 +133,25 @@ function App() {
             <Header/>
           </Grid>
           <Grid item container style={{margin:"1rem"}}>
-            <Grid item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
-            <Grid item xs={12} sm={8} md={6} lg={6} xl={4}>
-                <Webcam hidden ref={webcamRef} style={{zIndex:9}}/>
-                <canvas className="responsive-canvas" ref={canvasRef} style={{zIndex:9}}>
-                </canvas>
+            <Grid item xs={0} sm={2} md={3} lg={3} xl={4}>
+            
             </Grid>
-            <Grid item xs={0} sm={2} md={3} lg={3} xl={4}></Grid>
+            <Grid item xs={12} sm={8} md={6} lg={6} xl={4}>
+                  <div className='outer-div'>
+                    <Webcam ref={webcamRef} className="webcam-wrapper" mirrored={true}/>
+                    <canvas hidden className="responsive-canvas" ref={canvasRef} style={{zIndex:9}}>
+                    </canvas>
+                    <Canvas className="canvas-wrapper">
+                      <Suspense fallback={null}>
+                        <Model position={[0,0,-3]}/>
+                        <OrbitControls />
+                      </Suspense>
+                    </Canvas>
+                  </div>
+
+            </Grid>
+            <Grid item xs={0} sm={2} md={3} lg={3} xl={4}>
+            </Grid>
           </Grid>
       </Grid>
       </ThemeProvider>
